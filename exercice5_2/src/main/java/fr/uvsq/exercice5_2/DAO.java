12764e7633f0d25a8179ac97f863f70950032378
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-05-01 14:39:33 +0200

    modifier DAO en utilisant API JDBC : SGBD debry

--- exercice5_2/src/main/java/fr/uvsq/exercice5_2/DAO.java
+++ exercice5_2/src/main/java/fr/uvsq/exercice5_2/DAO.java
@@ -1,13 +1,43 @@
 package fr.uvsq.exercice5_2;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public interface DAO <T>{
-	//crud
-	public T create(T obj) throws IOException;
-    public T read(Integer id) throws ClassNotFoundException, IOException;
-    public T update(T obj) throws ClassNotFoundException, IOException;
-    public T delete(Integer id) throws IOException;
+public  abstract class DAO <T>{
 	
+	//crud
+	protected Connection connection=null;
+	String databaseURL = "jdbc:derby:my_db;create=true";
+	public abstract T create(T obj) throws IOException;
+    public abstract T read(Integer id) throws ClassNotFoundException, IOException;
+    public abstract T update(T obj) throws ClassNotFoundException, IOException;
+    public abstract T delete(Integer id) throws IOException;
+	
+    // la creation de la connexion avec un SGBD derby en mode embarqué
+    public void createConnection() {
+    	try
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            //Get a connection
+            connection = DriverManager.getConnection(databaseURL); 
+        }
+        catch (Exception except)
+        {
+            except.printStackTrace();
+        }
+    }
+    
+    //fermer la connection
+    public  void closeConnexion()
+    {
+    	try {
+    	      connection.close();
+    	    } catch (SQLException e) {
+    	      e.printStackTrace();
+    	    }
+
+    }
 
 }
