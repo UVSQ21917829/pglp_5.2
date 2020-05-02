32b810e5ef9b74b6b0874b05a67037273af3de71
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-05-02 14:08:09 +0200

    implementation GroupeDAO pour JDBC

--- exercice5_2/src/main/java/fr/uvsq/exercice5_2/GroupeDAO.java
+++ exercice5_2/src/main/java/fr/uvsq/exercice5_2/GroupeDAO.java
@@ -5,42 +5,115 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class GroupeDAO extends JdbcDAO<CompositePersonnels> {
 	
 	public CompositePersonnels create(CompositePersonnels obj) throws IOException {
 		// TODO Auto-generated method stub
 		this.createConnection();
-		FileOutputStream fout = new FileOutputStream("groupe.ser") ;
-    	ObjectOutputStream out = new ObjectOutputStream(fout) ;
-    	out.writeObject(obj) ; 
-    	out.close() ;
+		
+		try {
+		PreparedStatement prepareGroup = this.connection.prepareStatement("INSERT INTO GROUPE (id) VALUES(?)");
+		prepareGroup.setInt(2,obj.getId());
+		prepareGroup.executeUpdate();
+		PreparedStatement prepareEstDansPersonnal = this.connection.prepareStatement("INSERT INTO DANSPERSONNEL (id,id_p) VALUES(?)");
+		PreparedStatement prepareEstDansGroup = this.connection.prepareStatement("INSERT INTO DANSGROUP (id,id_g) VALUES(?)");
+
+		for (InterfacePersonnel composite : obj.listperso ) {
+
+	        if (composite instanceof Personnel) {
+	          personnelDAO pd= new personnelDAO();
+	          pd.create((Personnel) composite);
+	          prepareEstDansPersonnal.setInt(1, obj.getId());
+	          prepareEstDansPersonnal.setInt(2, ((Personnel) composite).getId());
+	          prepareEstDansPersonnal.executeUpdate();
+
+	        } else if (obj.getId()!=((CompositePersonnels) composite).getId()&&composite instanceof CompositePersonnels ) {
+	          this.create((CompositePersonnels) composite);
+	          prepareEstDansGroup.setInt(1, obj.getId());
+	          prepareEstDansGroup.setInt(2, ((CompositePersonnels) composite).getId());
+	          prepareEstDansGroup.executeUpdate();
+	        }
+	      
+		}
+	}catch (SQLException e) {
+		// TODO: handle exception
+		e.printStackTrace();
+	}
+	this.closeConnexion();
+		
 		return obj;
 	}
 
 	public CompositePersonnels read(Integer id) throws IOException, ClassNotFoundException {
-		CompositePersonnels groupe;
-		FileInputStream fileInput = new FileInputStream("groupe.ser");
-        ObjectInputStream objectInputStream = new ObjectInputStream(fileInput);
-        
-        groupe = (CompositePersonnels) objectInputStream.readObject();
-  
-		return groupe;
+		
+		CompositePersonnels gr=null;
+		try {
+			  PreparedStatement preapareGroupe =this.connection.prepareStatement("SELECT * FROM GROUPE  WHERE id = ?");
+	          PreparedStatement estdansperso =this.connection.prepareStatement("SELECT id_p FROM DANSPERSONNEL  WHERE id = ?");  
+	          PreparedStatement estdandgrop =this.connection.prepareStatement("SELECT id_g FROM DANSGROUPE  WHERE id =  ?");
+
+	          preapareGroupe.setInt(1, id);
+	          
+		      ResultSet ressultGroupeObj = preapareGroupe.executeQuery();
+		      while (ressultGroupeObj.next()) {
+		    	  gr = new CompositePersonnels(ressultGroupeObj.getInt("id"));
+		    	  //perso
+		    	  estdansperso.setInt(1, id);
+		          ResultSet resPerso = estdansperso.executeQuery();
+		          while (resPerso.next()) {
+		            personnelDAO pdao= new personnelDAO();
+		            gr.addPersonnel((Personnel) pdao.read(resPerso.getInt("id_p")));
+		          }
+	             //groupe
+		          estdandgrop.setInt(1, id);
+		         ResultSet resultGrp = estdandgrop.executeQuery();
+		          while (resultGrp.next()) {
+		           gr.addPersonnel((CompositePersonnels) this.read(resultGrp.getInt("id")));
+		        }
+	      }
+
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+
+	    this.closeConnexion();
+
+				
+		return gr;
 	}
 
 	public CompositePersonnels update(CompositePersonnels obj) throws IOException {
 		// TODO Auto-generated method stub
 		
-		FileOutputStream fout = new FileOutputStream("groupe.ser") ;
-    	ObjectOutputStream out = new ObjectOutputStream(fout) ;
-    	out.writeObject(obj) ; 
-    	out.close() ;
-		return obj;
+		return null;
 	}
 
-	public CompositePersonnels delete(Integer id) {
+	public void  delete(Integer id) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		try {
+			PreparedStatement preparedeleteG =this.connection.prepareStatement("DELETE FROM GROUPE WHERE id= ?"); 
+			PreparedStatement preparedeleteDansG =this.connection.prepareStatement("DELETE FROM DANSGROUPE WHERE id= ?"); 
+			PreparedStatement preparedeleteDansP =this.connection.prepareStatement("DELETE FROM DANSPERSONNEL WHERE id= ?"); 
+			
+			preparedeleteG.setInt(1, id);
+			preparedeleteG.executeUpdate();
+			preparedeleteDansG.setInt(1, id);
+			preparedeleteDansG.executeUpdate();
+			preparedeleteDansP.setInt(1, id);
+			preparedeleteDansP.executeUpdate();
+			
+		    } catch (SQLException e) {
+		      e.printStackTrace();
+		    }
+
+		    this.closeConnexion();;
+		
 	}
 
 }
